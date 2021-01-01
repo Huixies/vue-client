8eb571a7e8c4af2f159d3e0cbd7b5658888d918f
Huixies <1062240360@qq.com>
2021-01-01 17:38:26 +0800

    使用vue创建资金管理系统前台

---
+++ src/components/Dialog.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="dialog">
+        <el-dialog
+            title="添加基金信息"
+            :visible.sync="dialog.show"
+            :close-click-modal="false"
+            :close-on-press-escape="false"
+            :modal-append-to-body="false"
+        >
+            <div class="form">
+                <el-form
+                  ref="form"
+                  :model="formData"
+                  :rules="form_rules"
+                  label-width="120px"
+                  style="margin:10px;width:auto;"
+                >
+                  <el-form-item label="收支类型:">
+                      <el-select v-model="formData.type" placeholder="收支类型">
+                          <el-option v-for="(formtype,index) in format_type_list" :key="index" :label="formtype" :value="formtype">
+                          </el-option>
+                      </el-select>
+                  </el-form-item>
+                  <el-form-item prop="describe" label="收支描述:">
+                      <el-input type="describe" v-model="formData.describe"></el-input>
+                  </el-form-item>    
+                </el-form>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'diaLog',
+    data(){
+        return{
+            formData:{
+                type:"",
+                describe:"",
+                income:"",
+                expend:"",
+                cash:"",
+                remark:"",
+                id:""
+            },
+            format_type_list:[
+                "提现",
+                "提现手续费",
+                "充值",
+                "优惠券",
+                "充值礼券",
+                "转账"
+            ]
+        }
+    },
+    props:{
+        dialog:Object
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/views/FundList.vue
+++ src/views/FundList.vue
@@ -1,22 +1,37 @@
 <template>
-    <el-table
-    v-if="tableData.length>0"
-    :data="tableData"
-    max-height="450"
-    border
-    style="width: 100%">
-    <el-table-column
-        type="index"
-        label="序号"
-        align='center'
-        width="70"
-    >
-    </el-table-column>
+    <div class="fillContainer">
+      <div>
+        <el-form :inline="true" ref="add_data">
+        <el-form-item class="btnRight">
+          <el-button type="primary" size="small" icon="view" @click="handleAdd()">
+            添加
+          </el-button>
+        </el-form-item>
+        </el-form>
+      </div>
+      <div class="table_container">
+        <el-table
+        v-if="tableData.length>0"
+        :data="tableData"
+        max-height="450"
+        border
+        style="width: 100%">
+        <el-table-column
+            type="index"
+            label="序号"
+            align='center'
+            width="70"
+        >
+       </el-table-column>
        <el-table-column
          prop="date"
          label="创建时间"
          align="center"
          width="250">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
        </el-table-column>
        <el-table-column
          prop="type"
@@ -35,18 +50,27 @@
          label="收入"
          align="center"
          width="170">
+         <template slot-scope="scope">
+          <span style="color:#00d053">{{ scope.row.income }}</span>
+         </template>
        </el-table-column>
        <el-table-column
          prop="expend"
          label="支出"
          align="center"
          width="170">
+         <template slot-scope="scope">
+          <span style="color:#f56767">{{ scope.row.expend }}</span>
+         </template>
        </el-table-column>
        <el-table-column
          prop="cash"
          label="账户现金"
          align="center"
          width="170">
+         <template slot-scope="scope">
+          <span style="color:#4db3ff">{{ scope.row.cash }}</span>
+         </template>
        </el-table-column>
        <el-table-column
          prop="remark"
@@ -54,15 +78,42 @@
          align="center"
          width="220">
        </el-table-column>
-    </el-table>
+       <el-table-column 
+         prop="operation"
+         aligin="center"
+         label="操作"
+         fixed="right"
+         width="320"
+         >
+         <template slot-scope="scope">
+           <el-button
+             type="warning"
+             size="small"
+             icon="edit"
+             @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+           <el-button
+             type="danger"
+             size="small"
+             icon="delete"
+             @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+         </template>
+    </el-table-column>
+        </el-table>
+      </div>
+      <Dialog :dialog="dialog"></Dialog>
+    </div>
 </template>
 
 <script>
+import Dialog from '../components/Dialog';
 export default {
     name:'fundList',
     data(){
         return {
-            tableData:[]
+            tableData:[],
+            dialog:{
+              show:false
+            }
         }
     },
     created(){
@@ -75,11 +126,31 @@ export default {
             .then(res=>{
                 this.tableData = res.data;
             }).catch(err=>console.log(err))
+        },
+        handleAdd(){
+          this.dialog.show=true;
+        },
+        handleEdit(index,row){
+          console.log(123);
+        },
+        handleDelete(){
+          console.log(456);
         }
+    },
+    components:{
+      Dialog
     }
 }
 </script>
 
 <style scoped>
-
+.fillContainer{
+  width: 100%;
+  height: 100%;
+  padding: 16px;
+  box-sizing: border-box;
+}
+.btnRight{
+  float:right;
+}
 </style>
