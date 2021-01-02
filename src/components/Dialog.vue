5c33d1c218e419888f09b4d371ca0e5751a73109
Huixies <1062240360@qq.com>
2021-01-02 15:25:14 +0800

    使用vue创建资金管理系统前台

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="dialog">
         <el-dialog
-            title="添加基金信息"
+            :title="dialog.title"
             :visible.sync="dialog.show"
             :close-click-modal="false"
             :close-on-press-escape="false"
@@ -23,7 +23,27 @@
                   </el-form-item>
                   <el-form-item prop="describe" label="收支描述:">
                       <el-input type="describe" v-model="formData.describe"></el-input>
-                  </el-form-item>    
+                  </el-form-item>
+                  <el-form-item prop="income" label="收入:">
+                      <el-input type="income" v-model="formData.income"></el-input>
+                  </el-form-item> 
+                  <el-form-item prop="expend" label="支出:">
+                      <el-input type="expend" v-model="formData.expend"></el-input>
+                  </el-form-item> 
+                  <el-form-item prop="cash" label="账户现金:">
+                      <el-input type="cash" v-model="formData.cash"></el-input>
+                  </el-form-item>      
+                  <el-form-item prop="remark" label="备注:">
+                      <el-input type="textarea" v-model="formData.remark"></el-input>
+                  </el-form-item> 
+                  <el-form-item class="text_right">
+                      <el-button @click="dialog.show = false">
+                          取消
+                      </el-button>
+                       <el-button type="primary" @click="onSubmit('form')">
+                          提交
+                      </el-button>
+                  </el-form-item>
                 </el-form>
             </div>
         </el-dialog>
@@ -35,15 +55,6 @@ export default {
     name:'diaLog',
     data(){
         return{
-            formData:{
-                type:"",
-                describe:"",
-                income:"",
-                expend:"",
-                cash:"",
-                remark:"",
-                id:""
-            },
             format_type_list:[
                 "提现",
                 "提现手续费",
@@ -51,11 +62,47 @@ export default {
                 "优惠券",
                 "充值礼券",
                 "转账"
-            ]
+            ],
+            form_rules:{
+                describe:[
+                    {required:true,message:'收支描述不能为空！',trigger:'blur'}
+                ],
+                income:[
+                    {required:true,message:'收入不能为空',trigger:'blur'}
+                ],
+                expend:[
+                    {required:true,message:'支出不能为空',trigger:'blur'}
+                ],
+                cash:[
+                    {required:true,message:'账户现金不能为空！',trigger:'blur'}
+                ]
+            }
+        }
+    },
+    methods: {
+        onSubmit(form){
+            this.$refs[form].validate(valid=>{
+                if(valid){
+                    const url = this.dialog.option=='add'?'add':`edit/${this.formData.id}`
+                    this.$axios.post(`/api/profiles/${url}`,this.formData)
+                    .then(res=>{
+                        //添加成功
+                        this.$message({
+                            message:"数据添加成功",
+                            type:"success"
+                        });
+
+                        //隐藏dialog
+                        this.dialog.show = false;
+                        this.$emit('update');
+                    });
+                }
+            })
         }
     },
     props:{
-        dialog:Object
+        dialog:Object,
+        formData:Object
     }
 }
 </script>
--- src/views/FundList.vue
+++ src/views/FundList.vue
@@ -1,9 +1,33 @@
 <template>
     <div class="fillContainer">
       <div>
-        <el-form :inline="true" ref="add_data">
+        <el-form :inline="true" ref="search_data" :model="search_data">
+        <!-- 筛选 -->
+        <el-form-item label="按照时间筛选:">
+          <el-date-picker
+            v-model="search_data.startTime"
+            type="datetime"
+            placeholder="选择开始时间">
+          </el-date-picker>
+          --
+           <el-date-picker
+            v-model="search_data.endTime"
+            type="datetime"
+            placeholder="选择结束时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="small" icon="search" @click="handleSearch()">
+            筛选
+          </el-button>
+        </el-form-item>
         <el-form-item class="btnRight">
-          <el-button type="primary" size="small" icon="view" @click="handleAdd()">
+          <el-button type="primary" 
+                     size="small"
+                     icon="view"
+                     v-if="user.identity == 'manager'"
+                     @click="handleAdd()"
+          >
             添加
           </el-button>
         </el-form-item>
@@ -84,6 +108,7 @@
          label="操作"
          fixed="right"
          width="320"
+         v-if="user.identity == 'manager'"
          >
          <template slot-scope="scope">
            <el-button
@@ -99,8 +124,24 @@
          </template>
     </el-table-column>
         </el-table>
+        <!-- 分页 -->
+        <el-row>
+          <el-col :span="24">
+            <div class="pagination">
+              <el-pagination
+                  @size-change="handleSizeChange"
+                  @current-change="handleCurrentChange"
+                  :current-page.sync="paginations.page_index"
+                  :page-sizes="paginations.page_sizes"
+                  :page-size="paginations.page_size"
+                  :layout="paginations.layout"
+                  :total="paginations.total">
+              </el-pagination>
+            </div>
+          </el-col>
+        </el-row>
       </div>
-      <Dialog :dialog="dialog"></Dialog>
+      <Dialog :dialog="dialog" :formData='formData' @update="getProfile"></Dialog>
     </div>
 </template>
 
@@ -110,11 +151,40 @@ export default {
     name:'fundList',
     data(){
         return {
-            tableData:[],
-            dialog:{
-              show:false
-            }
+          search_data:{
+            startTime:'',
+            endTime:''
+          },
+          filterTableData:[],
+          paginations:{
+            page_index:1, //当前位于哪页
+            total:0,
+            page_size:5,  //每页显示多少条
+            page_sizes:[5,10,15,20],  //每页显示多少条
+            layout:'total,sizes,prev,pager,next,jumper'  //翻页属性
+          },
+          tableData:[],
+          allTableData:[],
+          dialog:{
+            show:false,
+            title:'',
+            option:'edit'
+          },
+          formData:{
+              type:"",
+              describe:"",
+              income:"",
+              expend:"",
+              cash:"",
+              remark:"",
+              id:""
+          },
         }
+    },
+    computed:{
+      user(){
+        return this.$store.getters.user;
+      }
     },
     created(){
         this.getProfile();
@@ -124,17 +194,106 @@ export default {
             //获取表格数据
             this.$axios('/api/profiles')
             .then(res=>{
-                this.tableData = res.data;
+                this.allTableData = res.data;
+                this.filterTableData = res.data;
+                //设置分页数据
+                this.setPaginations();
             }).catch(err=>console.log(err))
         },
+        setPaginations(){
+          //分页属性设置
+          this.paginations.total = this.allTableData.length
+          this.paginations.page_index = 1;
+          this.paginations.page_size = 5;
+          //设置默认的分页数据
+          this.tableData = this.allTableData.filter((item,index)=>{
+            return index < this.paginations.page_size;
+          });
+        },
         handleAdd(){
-          this.dialog.show=true;
+          this.dialog={
+            show:true,
+            title:"添加资金信息",
+            option:'add'
+          };
+
+          this.formData={
+            type:"",
+            describe:"",
+            income:"",
+            expend:"",
+            cash:"",
+            remark:"",
+            id:""
+          }
         },
         handleEdit(index,row){
-          console.log(123);
+          //编辑
+          this.dialog={
+            show:true,
+            title:"修改资金信息",
+            option:'edit'
+          };
+          this.formData={
+            type:row.type,
+            describe:row.describe,
+            income:row.income,
+            expend:row.expend,
+            cash:row.cash,
+            remark:row.remark,
+            id:row._id
+          }
         },
-        handleDelete(){
-          console.log(456);
+        handleDelete(index,row){
+          this.$axios.delete(`/api/profiles/delete/${row._id}`)
+          .then(res=>{
+            this.$message('删除成功!');
+            this.getProfile();
+          })
+        },
+        handleSizeChange(page_size){
+          //切换size
+          this.paginations.page_index = 1;
+          this.paginations.page_size = page_size;
+          this.tableData = this.allTableData.filter((item,index)=>{
+            return index < page_size;
+          });
+        },
+        handleCurrentChange(page){
+          //获取当前页
+          let index = this.paginations.page_size*(page-1);
+          //数据的总数
+          let nums = this.paginations.page_size*page
+          //容器
+          let tables = [];
+
+          for(let i=index;i<nums;i++){
+            if(this.allTableData[i]){
+              tables.push(this.allTableData[i]);
+            }
+            this.tableData =tables;
+          }
+        },
+        handleSearch(){
+          //筛选
+          if(!this.search_data.startTime || !this.search_data.endTime){
+            this.$message({
+              type:'warning',
+              message:'请选择时间区间'
+            });
+            this.getProfile();
+            return;
+          }
+          const sTime=this.search_data.startTime.getTime();
+          const eTime=this.search_data.endTime.getTime();
+          
+          this.allTableData = this.filterTableData.filter(item=>{
+              let date = new Date(item.date);
+              let time = date.getTime();
+              return time >=sTime && time <=eTime;
+            });
+          //分页数据
+          this.setPaginations();
         }
     },
     components:{
@@ -152,5 +311,9 @@ export default {
 }
 .btnRight{
   float:right;
+}
+.pagination{
+  text-align:right;
+  margin-top: 10px;
 }
 </style>
